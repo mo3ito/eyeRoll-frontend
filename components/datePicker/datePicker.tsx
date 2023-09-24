514fa6565baaba73db3361a8bb5f0860836ef94e
mo3ito <mostafa.tar90@gmail.com>
2023-09-24 22:22:24 +0330

    add date-picker

---
+++ components/datePicker/datePicker.tsx
@@ -0,0 +1,37 @@
+'use client'
+import {useState} from 'react'
+import DatePicker from "react-datepicker";
+import { format } from "date-fns";
+
+export default function DatesPicker() {
+
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(null);
+    
+
+    const handleDateChange = async (date)=>{
+    //    await setStartDate(date)
+    //    if(date){
+    //     const formattedDate = format(date, "dd/MM/yyyy"); // تبدیل تاریخ به فرمت "18/12/2022"
+    //   console.log("تاریخ به صورت فرمت دلخواه: ", formattedDate);
+    //    }
+    const [start, end] = date;
+    setStartDate(start);
+    setEndDate(end);
+    }
+    console.log("start", startDate);
+    console.log("end", endDate);
+    
+    
+  return (
+    <DatePicker
+    
+   selected={startDate}
+   onChange={(date) =>handleDateChange(date)}
+   startDate={startDate}
+   endDate={endDate}
+   selectsRange
+   inline
+ />
+  )
+}

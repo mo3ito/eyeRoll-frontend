5d25fdcefd53c8aa91c62f655a3adec1f92375c6
mo3ito <mostafa.tar90@gmail.com>
2023-10-02 22:31:50 +0330

    add type date picker

--- components/datePicker/datePicker.tsx
+++ components/datePicker/datePicker.tsx
@@ -1,14 +1,21 @@
 'use client'
-import {useState} from 'react'
+import {Dispatch, SetStateAction, useState} from 'react'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
+interface DatesPickerProps {
+isInline : boolean;
+isButton: boolean;
+startDate : Date ;
+setStartDate : Dispatch<SetStateAction<string>>;
+endDate : Date | null;
+setEndDate: Dispatch<SetStateAction<string>>;
+}
 
 
-export default function DatesPicker({isInline = true , isButton = true , startDate , setStartDate ,endDate , setEndDate }) {
+export default function DatesPicker({isInline = true , isButton = true , startDate , setStartDate ,endDate , setEndDate }: DatesPickerProps) {
 
-    // const [startDate, setStartDate] = useState(new Date());
-    // const [endDate, setEndDate] = useState(null);
+   
   
     
 
@@ -41,18 +48,7 @@ export default function DatesPicker({isInline = true , isButton = true , startDa
    
  />
 
-{/* <DatePicker
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      showTimeSelect
-      excludeTimes={[
-        setHours(setMinutes(new Date(), 0), 17),
-        setHours(setMinutes(new Date(), 30), 18),
-        setHours(setMinutes(new Date(), 30), 19),
-        setHours(setMinutes(new Date(), 30), 17),
-      ]}
-      dateFormat="MMMM d, yyyy h:mm aa"
-    /> */}
+
 {isButton && <button className='bg-indigo-400 w-full h-10 hover:bg-indigo-500 text-white rounded-md -translate-y-2 hover:font-semibold '>confirmation</button>}
 
 </>

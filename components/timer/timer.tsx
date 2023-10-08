b52de289dcf347c93fc3b5733957d5bc314fcbfc
mo3ito <mostafa.tar90@gmail.com>
2023-10-08 16:00:53 +0330

    fixed bug in Timer

--- components/timer/timer.tsx
+++ components/timer/timer.tsx
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from 'react';
 
 interface TimerProps {
-  timeMinutes:number;
-  className:string;
-  classNameTime:string
+  timeMinutes: number;
+  className: string;
+  classNameTime: string;
 }
 
 const COUNTER_KEY = 'timerCount';
 
+export default function Timer({ timeMinutes, className, classNameTime }: TimerProps) {
+  const [countdown, setCountdown] = useState<number>(timeMinutes * 60 || 400);
 
-export default function Timer({timeMinutes , className , classNameTime}: TimerProps) {
-
-  const [countdown, setCountdown] = useState<number>(timeMinutes*60 || 400);
- 
-  
 
   useEffect(() => {
     // بازیابی مقدار تایمر از localStorage
-    const storedCountdown = parseInt(localStorage.getItem(COUNTER_KEY), 10);
-
-    if (!isNaN(storedCountdown)) {
-      setCountdown(storedCountdown);
+    const storedCountdown = localStorage.getItem(COUNTER_KEY);
+  
+    if (storedCountdown !== null) {
+      const parsedCountdown = parseInt(storedCountdown, 10);
+      if (!isNaN(parsedCountdown)) {
+        setCountdown(parsedCountdown);
+      }
     }
   }, []);
 
@@ -29,7 +29,7 @@ export default function Timer({timeMinutes , className , classNameTime}: TimerPr
       setCountdown((prevCountdown) => {
         if (prevCountdown > 0) {
           // ذخیره مقدار تایمر در localStorage
-          localStorage.setItem(COUNTER_KEY, prevCountdown - 2);
+          localStorage.setItem(COUNTER_KEY, (prevCountdown - 2).toString());
           return prevCountdown - 1;
         }
 
@@ -40,20 +40,34 @@ export default function Timer({timeMinutes , className , classNameTime}: TimerPr
       });
     }, 1000);
 
-    return () => clearInterval(timer);
+    return () => clearInterval(timer); // حذف تایمر در هنگام unmount کردن کامپوننت
   }, []);
 
-  const formatTime = (time : number) => {
-    const minutes = Math.floor(time / 60);
+  useEffect(() => {
+    // از این مکان می‌توانید اقدامات دیگری که نیاز به انجام آنها در هنگام تغییر timeMinutes دارید، انجام دهید.
+
+    // به عنوان مثال، اگر می‌خواهید timeMinutes را تغییر دهید، می‌توانید مقدار countdown را مجدداً تنظیم کنید.
+
+    setCountdown(timeMinutes * 60 || 400);
+  }, [timeMinutes]);
+
+  const formatTime = (time: number) => {
+    const days = Math.floor(time / (60 * 60 * 24));
+    const hours = Math.floor((time % (60 * 60 * 24)) / (60 * 60));
+    const minutes = Math.floor((time % (60 * 60)) / 60);
     const seconds = time % 60;
-    const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
-    const formattedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
-    return `${formattedMinutes}:${formattedSeconds}`;
+
+    const formattedDays = days > 0 ? `${days}d ` : '';
+    const formattedHours = hours > 0 ? `${hours}h ` : '';
+    const formattedMinutes = minutes > 0 ? `${minutes}m ` : '';
+    const formattedSeconds = seconds > 0 ? `${seconds}s` : '';
+
+    return `${formattedDays}${formattedHours}${formattedMinutes}${formattedSeconds}`;
   };
 
   return (
-    <div className={className}>
-      <p className={classNameTime}>{formatTime(countdown)}</p>
+    <div className={`${className} `}>
+      <div className={classNameTime}>{formatTime(countdown)}</div>
     </div>
   );
 }

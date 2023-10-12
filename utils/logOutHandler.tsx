6cb61a4184c0d925a284ea44d2838847b38e94eb
mo3ito <mostafa.tar90@gmail.com>
2023-10-12 18:51:59 +0330

    add type for logOutHandler

--- utils/logOutHandler.tsx
+++ utils/logOutHandler.tsx
@@ -1,7 +1,8 @@
 import EYEROLL_TOKEN from "@/help/tokenName";
 import Cookies from "js-cookie";
+import { NextRouter } from "next/router";
 
-const logOutHandler = async (router : string) => {
+const logOutHandler = async (router : NextRouter) => {
   await Cookies.remove(EYEROLL_TOKEN);
   router.push("/");
 };

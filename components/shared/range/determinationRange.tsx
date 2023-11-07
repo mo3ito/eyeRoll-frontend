661464b9760b48d199badbf71f37c0be592ec551
mo3ito <mostafa.tar90@gmail.com>
2023-11-07 12:08:46 +0330

    responsive discount on all products

--- components/shared/range/determinationRange.tsx
+++ components/shared/range/determinationRange.tsx
@@ -34,7 +34,7 @@ export default function DeterminationRange({minValue , maxValue , setMinValue , 
   return (
     <div className={` ${isChecked ? ' bg-indigo-100' : 'bg-gray-200'} w-full h-max  rounded-xl my-2  p-2`}>
     <div className='flex  items-start justify-center   '>
-      <div className='  bg-gray-300 w-10/12'>
+      <div className='w-10/12'>
        <p className="inline-block max-xs:text-xs text-sm  sm:text-lg font-medium "><InformationButton onClick={showInformation}/> {title}</p>
       </div>
     
@@ -48,21 +48,22 @@ export default function DeterminationRange({minValue , maxValue , setMinValue , 
           circleClasses=" max-xs:w-2 max-xs:h-2 max-xs:peer-checked:translate-x-[10px] w-3 h-3 sm:w-4 sm:h-4 bg-gray-200 peer-checked:translate-x-3 sm:peer-checked:translate-x-6  peer-checked:bg-violet-500"
         />
     </div>
-    <div className='w-full  h-max  rounded-xl space-y-4 flex items-center justify-center flex-col mt-3'>
-        <div className='  space-x-10 flex items-center justify-center w-full'>
-          <div className=' '>
-          <span className='text-lg pr-1'>min</span>
-          <div className='w-24  h-10 border rounded-lg border-purple-300 shadow-md inline-block'>
-          <input disabled={!isChecked} value={ !isChecked ? 0 : minValue} onChange={addHandler} type="number" className=' bg-transparent w-16 h-10  rounded-lg  pl-3  outline-none'/>
-          %
+    <div className='w-full h-max  rounded-xl space-y-4 flex items-center justify-center flex-col mt-3 -translate-y-2 sm:translate-y-0'>
+        <div className=' space-x-4 max-xs:space-x-2  sm:space-x-10 flex  items-center justify-center w-full'>
+          <div className=''>
+          <span className=' pl-6  max-xs:text-base max-xs:pl-4 sm:pl-8 text-lg '>min</span>
+          <div className=' flex items-center justify-center  max-xs:w-16 max-xs:h-8 w-20 h-9   sm:w-24  sm:h-10 border rounded-lg border-purple-300 shadow-md '>
+          <input disabled={!isChecked} value={ !isChecked ? 0 : minValue} onChange={addHandler} type="number" className='w-14 max-xs:text-sm bg-transparent max-xs:w-12  pl-2  sm:w-16 sm:h-10  rounded-lg  sm:pl-3  outline-none'/>
+          <span className='pr-2'>%</span>
+         
           </div>
           
           </div>
-          <div>
-          <span className='text-xl pr-1 ml-3'>max</span>
-          <div className='w-24  h-10 border rounded-lg border-purple-300 shadow-md inline-block'>
-          <input disabled={!isChecked} value={ !isChecked ? 0 : maxValue} onChange={minusHandler} type="number" className=' bg-transparent w-16 h-10  rounded-lg  pl-3  outline-none '/>
-          %
+          <div className=''>
+          <span className=' pl-6  max-xs:text-base max-xs:pl-4 sm:pl-8 text-lg '>max</span>
+          <div className=' flex items-center justify-center  max-xs:w-16 max-xs:h-8 w-20 h-9   sm:w-24  sm:h-10 border rounded-lg border-purple-300 shadow-md '>
+          <input disabled={!isChecked} value={ !isChecked ? 0 : maxValue} onChange={minusHandler} type="number" className='w-14 max-xs:text-sm bg-transparent max-xs:w-12  pl-2  sm:w-16 sm:h-10  rounded-lg  sm:pl-3  outline-none'/>
+          <span className='pr-2'>%</span>
           </div>
           </div>
           

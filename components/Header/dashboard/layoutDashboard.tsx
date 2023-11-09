49b460f77cf088e94a8677627af0085bd431d739
mo3ito <mostafa.tar90@gmail.com>
2023-11-09 12:56:33 +0330

    responsive header in dashboard

--- components/Header/dashboard/layoutDashboard.tsx
+++ components/Header/dashboard/layoutDashboard.tsx
@@ -14,6 +14,7 @@ const LayoutDashboard = () => {
   const [showAside, setShowAside] = useState<boolean>(false);
   const leftMenuRef = useRef<HTMLDivElement | null>(null);
   const [showBox , setShowBox] = useState<boolean>(false)
+  const [showSwitchAccount , setShowSwitchAccount]=useState<boolean>(false)
   const { infos } = useContext(AuthContext);
   const router = useRouter()
 
@@ -42,12 +43,12 @@ const LayoutDashboard = () => {
   return (
 <>
     <header className='h-24 bg-gradient-to-b from-indigo-300 to-sky-100   flex items-center justify-center '>
-    <div className=' w-full h-full mx-auto  flex items-center justify-between  px-20 '>
+    <div className=' w-full h-full  mx-auto  flex items-center justify-between max-xs:px-2 px-10 sm:px-20 '>
    
-    <div className='flex items-center   text-xl h-full space-x-12 w-full '>
+    <div className='flex items-center   text-xl h-full space-x-3  w-full '>
     
         <button
-          className="iniline-block text-xl   mr-8"
+          className="iniline-block text-xl   sm:mr-8"
           onClick={() => setShowAside(true)}
         >
           <svg
@@ -68,25 +69,25 @@ const LayoutDashboard = () => {
         <span className=' inline-block mr-1 text-stone-600'>{infos.username}</span>
         <svg className='w-4 h-4 fill-zinc-700 inline-block' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 16L6 10H18L12 16Z"></path></svg>
           </div>
-          <ul className={`hidden ${ showBox && 'group-hover:block'} text-lg absolute border p-1 border-purple-500 bg-blue-100 z-50 shadow-md  top-24 left-0 h-max  min-w-[176px] w-max  rounded-xl `}>
+          <ul className={`hidden ${ showBox && 'group-hover:block'} text-sm sm:text-lg absolute border p-1 border-purple-500 bg-blue-100 z-50 shadow-md  top-24 left-0 h-max w-52 sm:min-w-[176px] sm:w-max  rounded-xl `}>
            
-            <li className='px-6 py-2 group/item relative hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white '>
+            <li onMouseEnter={()=>setShowSwitchAccount(true)} onClick={()=>setShowSwitchAccount(prev => !prev)} className=' px-2 sm:px-6 py-2 group/item relative hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white '>
             switch the other account
             <svg className="inline-block w-4 h-4 ml-2 fill-fuchsia-700 group-hover/item:fill-white " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16 12L10 18V6L16 12Z"></path></svg>
-            <ul className="absolute  hidden group-hover/item:block left-full min-w-xl max-w-max border border-purple-500 bg-blue-100 p-1 top-0 rounded-lg">
-              <li className="px-4 py-2  hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white">mostafa</li>
-              <li className="px-4 py-2  hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white">saj</li>
-              <li className="px-4 py-2  hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white">rez</li>
-            </ul>
+            { showSwitchAccount && <ul className="absolute  hidden group-hover/item:block top-8 w-44 sm:left-full sm:top-0 sm:min-w-xl sm:max-w-max border border-purple-500 bg-blue-100 p-1  rounded-lg">
+              <li className=" px-2 sm:px-4 py-2  hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white">mostafa</li>
+              <li className=" px-2 sm:px-4 py-2  hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white">saj</li>
+              <li className=" px-2 sm:px-4 py-2  hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white">rez</li>
+            </ul>}
               </li>
-                <li onClick={()=>logOutHandler(router)} className='px-6 py-2  hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white '>
+                <li onClick={()=>logOutHandler(router)} className=' px-2 sm:px-6 py-2  hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white '>
             log out
               </li>
           </ul>
           
         </li>
 
-        <HeaderTitleLi title="Nearest stores" options={[{id:"3" , name:"Ali cafe" , path:"/" } , {id:"4" , name:"joe black" , path:"/"  } ]} />
+        {/* <HeaderTitleLi title="Nearest stores" options={[{id:"3" , name:"Ali cafe" , path:"/" } , {id:"4" , name:"joe black" , path:"/"  } ]} /> */}
      
         </ul>
      

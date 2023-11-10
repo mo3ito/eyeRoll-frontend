656e01455b8b5e7d14e9bf60454807a0d5bb8f3d
mo3ito <mostafa.tar90@gmail.com>
2023-11-10 17:59:53 +0330

    responsive delete modal

--- components/modal/modal.tsx
+++ components/modal/modal.tsx
@@ -31,23 +31,26 @@ export default function Modal({text , isShowModal , setIsShowModal , confirmHand
       onClick={handleBackdropClick}
       className="w-screen h-screen  fixed flex items-center justify-center bg-black/40 backdrop-blur-sm  inset-0 z-50  "
     >
-      <div
+
+<div className="container max-xs:px-2   mx-auto flex items-center justify-center">
+<div
         ref={modalRef}
-        className=" relative bg-sky-50 w-64 sm:w-96 lg:w-[500px] h-64 flex justify-center items-center rounded-lg"
+        className=" relative bg-white -translate-y-20 text-sm sm:text-base max-xs:w-full w-11/12 sm:w-8/12 md:w-7/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 h-2/4 py-10  flex justify-center items-center rounded-lg"
       >
         <CloseIcon
           onClick={() => setIsShowModal(false)}
-          classNameButton="absolute top-3 right-3"
-          classNameSvg="w-8 h-8 fill-red-400"
+          classNameButton="absolute top-2 right-2 "
+          classNameSvg= "w-5 h-5 fill-red-400 sm:w-6 sm:h-6 lg:w-8 lg:h-8 "
         />
         <div className="w-full h-full flex flex-col items-center justify-center">
-       <p className="mb-6 text-lg">{text}</p>
+       <p className="mb-6 ">{text}</p>
        <div className=" w-full text-center">
-       <button onClick={cancelHandler}  className="inline-block bg-rose-300 hover:bg-rose-400  h-10 rounded-lg mr-2 w-1/3" >No</button>
-       <button onClick={confirmHandler}  className="inline-block  bg-green-300 hover:bg-green-400 h-10 rounded-lg ml-2 w-1/3" >Yes</button>
+       <button onClick={cancelHandler}  className="inline-block bg-rose-300 hover:bg-rose-400 h-8  sm:h-10 rounded-lg mr-2 w-1/3" >No</button>
+       <button onClick={confirmHandler}  className="inline-block  bg-green-300 hover:bg-green-400 h-8 sm:h-10 rounded-lg ml-2 w-1/3" >Yes</button>
        </div>
        
         </div>
+      </div>
       </div>
     </div>
   );

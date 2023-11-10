c0c8d96a2fb92d525d5afe10b3dcd722681888e9
mo3ito <mostafa.tar90@gmail.com>
2023-11-10 17:44:41 +0330

    responsive modal

--- components/modal/modalDefault.tsx
+++ components/modal/modalDefault.tsx
@@ -29,16 +29,20 @@ export default function ModalDefault({children , isShowModal , setIsShowModal , 
       onClick={handleBackdropClick}
       className="w-screen h-screen  fixed flex items-center justify-center bg-black/40 backdrop-blur-sm  inset-0 z-50 "
     >
+      <div className="container max-xs:px-2   mx-auto flex items-center justify-center">
+
+     
       <div
         ref={modalRef}
-        className=" relative bg-white w-4/12 h-2/4   flex justify-center items-center rounded-lg"
+        className=" relative bg-white text-sm sm:text-base max-xs:w-full w-11/12 sm:w-8/12 md:w-7/12 lg:w-6/12 xl:w-5/12 2xl:w-4/12 h-2/4   flex justify-center items-center rounded-lg"
       >
         <CloseIcon
           onClick={() => setIsShowModal(false)}
-          classNameButton="absolute top-3 right-3 "
-          classNameSvg={closeIconClassName}
+          classNameButton="absolute top-2 right-2 "
+          classNameSvg= "w-5 h-5 fill-red-400 sm:w-6 sm:h-6 lg:w-8 lg:h-8 "
         /> 
        <div className="w-full h-full" >{children}</div>
+       </div>
        </div>
     </div>
   );

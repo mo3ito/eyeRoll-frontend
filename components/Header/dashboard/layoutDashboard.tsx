aa1292f7e4494d2dc342badb253b23298cdb6e39
mo3ito <mostafa.tar90@gmail.com>
2023-11-12 17:33:36 +0330

    resize rechart for responsive

--- components/Header/dashboard/layoutDashboard.tsx
+++ components/Header/dashboard/layoutDashboard.tsx
@@ -17,10 +17,6 @@ const LayoutDashboard = () => {
   const router = useRouter()
 
 
-
-  
-
-
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
@@ -39,14 +35,12 @@ const LayoutDashboard = () => {
   }, [showAside]);
 
  
-
   return (
 <>
     <header className='h-24 bg-gradient-to-b from-indigo-300 to-sky-100 w-full fixed top-0 z-50  flex items-center justify-center '>
     <div className=' w-full h-full  mx-auto  flex items-center justify-between max-xs:px-2 px-10 sm:px-20 '>
    
     <div className='flex items-center   h-full space-x-3  w-full '>
-    
         <button
           className="iniline-block text-xl  mb-2 sm:mb-0 md:mr-8"
           onClick={() => setShowAside(true)}
@@ -59,9 +53,6 @@ const LayoutDashboard = () => {
             <path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"></path>
           </svg>
         </button>
-
-
-   
         <div onMouseLeave={()=>setShowBox(false)} onMouseEnter={()=>setShowBox(true)} className="w-max max-w-[200px] h-12 relative ">
           <div  onClick={()=>setShowBox(prev=> !prev)} className="w-max flex items-center justify-center cursor-pointer">
             <img className=" w-10 h-10 sm:w-12 sm:h-12 object-cover rounded-full inline-block" src={infos.profile_image_path ? infos.profile_image_path : "/images/defaultPerson.png"} alt="" />
@@ -75,7 +66,6 @@ const LayoutDashboard = () => {
             { !showSwitchAccount ? <svg className=" w-4 h-4  sm:w-5 sm:h-5 ml-4 sm:ml-5 fill-fuchsia-700 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 11.8284L9.17154 14.6569L7.75732 13.2426L12 9L16.2426 13.2426L14.8284 14.6569L12 11.8284Z"></path></svg>
            : <svg className="w-4 h-4  sm:w-5 sm:h-5 ml-4  sm:ml-5 fill-white inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12.1717 12.0005L9.34326 9.17203L10.7575 7.75781L15.0001 12.0005L10.7575 16.2431L9.34326 14.8289L12.1717 12.0005Z"></path></svg>}
              </span>
-           
             <ul className={` ${showSwitchAccount ? 'absolute' : 'hidden'} left-[10px] z-40 w-11/12 sm:left-60 sm:top-0 rounded-lg sm:w-36 h-max bg-blue-100 border border-fuchsia-400`}>
             <li className=" px-2 sm:px-4 py-2  hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white truncate">mostafasssssssssssssssssssssssssss</li>
               <li className=" px-2 sm:px-4 py-2  hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white truncate">saj</li>
@@ -83,7 +73,6 @@ const LayoutDashboard = () => {
             </ul>
             </li>
           <li onClick={()=>logOutHandler(router)} className=' cursor-pointer px-2 sm:px-2 py-2  relative hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white '>log out</li>
-         
           </ul>
           </div>
 

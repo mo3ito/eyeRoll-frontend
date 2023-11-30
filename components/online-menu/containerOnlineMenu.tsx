2c216faaa05275e64e794008d700ed11964474e8
mo3ito <mostafa.tar90@gmail.com>
2023-11-30 17:42:16 +0330

    responsive online menu

--- components/online-menu/containerOnlineMenu.tsx
+++ components/online-menu/containerOnlineMenu.tsx
@@ -22,11 +22,11 @@ export default function ContainerOnlineMenu({sortedProduct = [] , setIsShowProdu
  {sortedProduct.map((item : sortedProduct) =>
            <div key={item.id} className='w-full text-sm md:text-base bg-sky-50 rounded-lg h-max mb-2 px-4 pb-6 pt-2 border border-fuchsia-400'>
            <HeaderMenu content={item.group}/>
-           <div className='w-full  h-max flex justify-around flex-wrap gap-x-2 gap-y-3 sm:gap-y-8 pt-4'>
+           <div className='w-full  h-max flex justify-around flex-wrap gap-x-2 gap-y-3 sm:gap-y-4 pt-4'>
    
            {item.values.map((product : ProductType) =>
                <div onClick={()=>detailsHandler(product.productName , product.product_image_path , product.productPrice , product.productPricePetty , product.productDescription )} key={product._id} 
-               className=' cursor-pointer w-full sm:w-[280px] md:w-[350px] lg:w-[470px] xl:w-[400px] 2xl:w-[480px] h-28 md:h-32 lg:h-44 xl:h-40 2xl:h-44 border border-fuchsia-400 rounded-lg p-2 flex bg-indigo-50'>
+               className=' hoverScale cursor-pointer w-full sm:w-[280px] md:w-[350px] lg:w-[470px] xl:w-[400px] 2xl:w-[480px] h-28 md:h-32 lg:h-44 xl:h-40 2xl:h-44 border border-fuchsia-400 rounded-lg p-2 flex bg-indigo-50'>
                <div className=' w-[100px] sm:w-6/12 md:w-4/12 lg:w-5/12 h-full '>
                <img src={product.product_image_path} className=' w-full h-full object-cover' alt="" />
                </div>
--- components/online-menu/showDetailsOnlineMenu.tsx
+++ components/online-menu/showDetailsOnlineMenu.tsx
@@ -20,13 +20,15 @@ export default function ShowDetailsOnlineMenu({isShowProduct , setIsShowProduct 
     <p className='text-center mb-4 pt-2  text-fuchsia-400 font-semibold'>Product Specifications</p>
     <img className='bg-red-200 w-full h-64 object-cover hover:scale-105 hover:duration-500' src={productDetails?.productImage ? productDetails?.productImage : "/images/default-product.jpg"} alt="" />
     <div className=' mt-2 text-fuchsia-400 font-semibold'>
-    <p className='w-full h-max max-h-[96px]  overflow-y-auto border-b ' > <span className='text-black'>name:</span>  {productDetails?.producName}</p>
+    <p className='w-full h-max max-h-[96px]  overflow-y-auto border-b ' > <span className='text-black'>name:</span> {productDetails?.producName}</p>
     <p className='text-red-400 border-b'> <span className='text-black '>price:</span> {productDetails?.productPrice}.{productDetails?.productPricePetty ? productDetails?.productPricePetty : null } $</p>
     </div >
     <div className=''>
       <p className='text-center text-red-400 font-semibold'>details:</p>
-      <div className='w-full h-44 shadow-lg border overflow-y-auto p-2 break-words text-zinc-600'>
-      {productDetails?.productDescription}
+      <div className='w-full h-44 shadow-lg border overflow-y-auto p-2 break-words text-base text-zinc-600'>
+      
+      { productDetails?.productDescription ? <span>{productDetails?.productDescription}</span>
+     : <p className='w-full h-full flex items-center justify-center'>no description</p>}
       </div>
     </div>
     

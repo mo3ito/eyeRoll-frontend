dd00e663a760c2bd0d45cabbd45896dee8464ae8
mo3ito <mostafa.tar90@gmail.com>
2023-12-05 14:01:35 +0330

    add to top component

--- components/toTop/toTop.tsx
+++ components/toTop/toTop.tsx
@@ -25,8 +25,8 @@ export default function ToTop() {
     }
 
   return (
-    <button onClick={toTopHandler} className={`${showToTop ? 'block' : 'hidden'} bg-indigo-300 fixed   right-12 bottom-10 border border-fuchsia-400 rounded-full p-2 z-50`}>
-        <svg className='w-5 h-5 ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11.9997 10.8284L7.04996 15.7782L5.63574 14.364L11.9997 8L18.3637 14.364L16.9495 15.7782L11.9997 10.8284Z"></path></svg>
+    <button onClick={toTopHandler} className={`${showToTop ? 'block' : 'hidden'} bg-indigo-300 fixed max-xs:right-1  right-2  md:right-3  2xl:right-6 bottom-10 border border-fuchsia-400 rounded-full  p-1  md:p-2 z-50`}>
+        <svg className='w-3 h-3 sm:w-4 sm:h-4   2xl:w-5 2xl:h-5 ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11.9997 10.8284L7.04996 15.7782L5.63574 14.364L11.9997 8L18.3637 14.364L16.9495 15.7782L11.9997 10.8284Z"></path></svg>
     </button>
   )
 }

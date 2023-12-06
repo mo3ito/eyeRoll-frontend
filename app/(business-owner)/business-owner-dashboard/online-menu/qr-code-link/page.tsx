5d17b326d256d6e4db99cc8695b25525ced1bcab
mo3ito <mostafa.tar90@gmail.com>
2023-12-06 11:40:26 +0330

    add download online menu link

--- app/(business-owner)/business-owner-dashboard/online-menu/qr-code-link/page.tsx
+++ app/(business-owner)/business-owner-dashboard/online-menu/qr-code-link/page.tsx
@@ -3,32 +3,68 @@ import {useState , useEffect , useContext} from 'react'
 import { AuthContext } from '@/context/authContext';
 import useGetBusinessOwnerId from '@/hooks/useGet‌‌BusinessOwnerId';
 import QRCode from 'qrcode.react';
+import { toast } from 'react-toastify';
+import { InfosProps } from '@/types/authentication';
 
 export default function QRCodeLink() {
 
     const [qrCodeData, setQRCodeData] = useState('');
     const {infos}= useContext(AuthContext)
-    const{businessOwnerId} = useGetBusinessOwnerId(infos)
-  
-    console.log(businessOwnerId);
-    
+    const{businessOwnerId} = useGetBusinessOwnerId(infos as InfosProps)
   
     useEffect(() => {
-      // این قسمت مربوط به ساخت داده‌هایی است که می‌خواهید در QR code قرار دهید.
-      const qrData = `http://localhost:3000/online-menu/${businessOwnerId}`;
-  
-      setQRCodeData(qrData);
+      if(businessOwnerId){
+        const path = `${process.env.NEXT_PUBLIC_BASE_URL}/online-menu/${businessOwnerId}`
+        setQRCodeData(path);
+      }
     }, [businessOwnerId]);
 
+    const handleCopyToClipboard = async ()=>{
+      if(navigator.clipboard && qrCodeData){
+        try {
+         await navigator.clipboard.writeText(qrCodeData)
+         toast.success('Link copied!')
+        } catch (error) {
+          console.error('Copy to clipboard failed: ', error);
+        }
+      }
+    }
+
+
+    const handleDownload = () => {
+      const canvas = document.getElementById('qrcode')?.getElementsByTagName('canvas')[0];
+      canvas?.toBlob(function(blob) {
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'qrcode.png';
+        link.click();
+      });
+    };
+    
+
   return (
-    <div className='mt-44  w-max px-6 mx-auto '>
-        <h1 className='text-center mb-20'>your QR Code</h1>
-        { qrCodeData && <QRCode className=' max-xs:!w-28 max-xs:!h-28 !w-36 !h-36 md:!w-40 md:!h-40 lg:!w-44 lg:!h-44 xl:!w-48 xl:!h-48 2xl:!w-52 2xl:!h-52' size={300} value={qrCodeData}  imageSettings={{
+    <div className='mt-64  w-max px-6 mx-auto border flex items-center justify-center flex-col gap-y-3 py-6 shadow-lg rounded-lg border-fuchsia-400'>
+       
+        { qrCodeData && 
+            <div id='qrcode'>
+        <QRCode className=' max-xs:!w-28 max-xs:!h-28 !w-36 !h-36 md:!w-40 md:!h-40 lg:!w-44 lg:!h-44 xl:!w-48 xl:!h-48 2xl:!w-52 2xl:!h-52' size={300} value={qrCodeData}  imageSettings={{
             src:  "/images/mo3ito.jpg",
-            height: 30, // ارتفاع لوگو
-            width: 30, // عرض لوگو
-            excavate: true, // برش قسمت‌های لوگو که درون کد قرار گیرد
-          }}/>}
+            height: 30, 
+            width: 30, 
+            excavate: true,
+          }}/>
+          </div>
+          }
+          
+          <button className='border border-fuchsia-400 p-2 rounded-lg cursor-pointer' onClick={handleDownload}>
+        Download QR Code
+      </button>
+
+          <div className='mt-4'>
+          <p className='text-center font-semibold mb-1'>your link</p>
+          <button title='click to copy' onClick={handleCopyToClipboard} className='border bg-fuchsia-400 border-fuchsia-400 p-2 rounded-lg cursor-pointer'>{`${process.env.NEXT_PUBLIC_BASE_URL}/online-menu/${businessOwnerId}`}</button>
+          </div>
+        
 
          
 
--- components/toTop/toTop.tsx
+++ components/toTop/toTop.tsx
@@ -1,32 +1,42 @@
-'use client'
-import React, { useEffect, useState } from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
 
 export default function ToTop() {
-    const [showToTop , setShowToTop]=useState<boolean>(false)
-  
-    useEffect(()=>{
-        const toggleVisibility = ()=>{
-            if(scrollY > 1300){
-                setShowToTop(true)
-            }else{
-                setShowToTop(false)
-            }
-        }
-        window.addEventListener("scroll", toggleVisibility)
-        return ()=> window.removeEventListener("scroll", toggleVisibility)
-    },[])
+  const [showToTop, setShowToTop] = useState<boolean>(false);
 
-    const toTopHandler = ()=>{
-        window.scrollTo({
-            top : 0,
-            behavior: "smooth"
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (scrollY > 1500) {
+        setShowToTop(true);
+      } else {
+        setShowToTop(false);
+      }
+    };
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
 
-        })
-    }
+  const toTopHandler = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
-    <button onClick={toTopHandler} className={`${showToTop ? 'block' : 'hidden'} bg-indigo-300 fixed max-xs:right-1  right-2  md:right-3  2xl:right-6 bottom-10 border border-fuchsia-400 rounded-full  p-1  md:p-2 z-50`}>
-        <svg className='w-3 h-3 sm:w-4 sm:h-4   2xl:w-5 2xl:h-5 ' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11.9997 10.8284L7.04996 15.7782L5.63574 14.364L11.9997 8L18.3637 14.364L16.9495 15.7782L11.9997 10.8284Z"></path></svg>
+    <button
+      onClick={toTopHandler}
+      className={`${
+        showToTop ? "block" : "hidden"
+      } bg-indigo-300 fixed max-xs:right-1  right-2  md:right-3  2xl:right-6 bottom-10 border border-fuchsia-400 rounded-full  p-1  md:p-2 z-50`}
+    >
+      <svg
+        className="w-3 h-3 sm:w-4 sm:h-4   2xl:w-5 2xl:h-5 "
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <path d="M11.9997 10.8284L7.04996 15.7782L5.63574 14.364L11.9997 8L18.3637 14.364L16.9495 15.7782L11.9997 10.8284Z"></path>
+      </svg>
     </button>
-  )
+  );
 }

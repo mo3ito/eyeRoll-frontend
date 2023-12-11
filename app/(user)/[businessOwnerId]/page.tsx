4439026cd314074efcda04e487b5b90c19ad0e92
mo3ito <mostafa.tar90@gmail.com>
2023-12-11 17:09:42 +0330

    add type on search bar

--- app/(user)/[businessOwnerId]/page.tsx
+++ app/(user)/[businessOwnerId]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page({params}:{params : {businessOwnerId : string}; searchParams: { search: string }}) {
 
     console.log(params);
-    console.log(searchParams);
+    
     
     
   return (

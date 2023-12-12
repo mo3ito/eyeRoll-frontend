dc3cd2a665184a777005581ce9a3f61cd5e35efb
mo3ito <mostafa.tar90@gmail.com>
2023-12-12 10:00:10 +0330

    add backend searher to frontend

--- components/containerResultSearch/containerResultSearch.tsx
+++ components/containerResultSearch/containerResultSearch.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 import useDropDownHandler from '@/hooks/useDropDownHandler'
+import getter from '@/services/getter'
 
 export default function ContainerResultSearch() {
     const [inputValue , setInputValue]=useState<string>("")
@@ -9,31 +10,31 @@ export default function ContainerResultSearch() {
     const boxSearchRef = useRef<HTMLDivElement | null>(null)
     useDropDownHandler(boxSearchRef , setIsShowResultBox)
 
-    const information = [
-        {name: "cafe farhang" },
-        {name: "cafe milad" },
-
-    ]
+   
     const changeInputValueHandler = (event : ChangeEvent<HTMLInputElement>)=>{
         setInputValue(event?.target.value)
     }
     useEffect(()=>{
-        if(inputValue.trim() !== ""){
-            
-                const infos = [...information]
-               const result = infos.filter(item=> item.name.startsWith(inputValue))
-               if(result.length >0){
-                setIsShowResultBox(true)
-                setResultSearch(result)
-               }else{
-                setIsShowResultBox(false)
-               }
-              
-        } else{
+        const searcher = async ()=>{
+            if(inputValue.trim() !== ""){
+                try {
+                 const response =  await getter(`http://localhost:5000/eyeRoll/search-information/?input_value=${inputValue}`)
+                 console.log(response);
+                 if(response?.status === 200){
+                    setIsShowResultBox(true)
+                    setResultSearch(response.data)
+                 }
+                 
+                } catch (error) {
+                    console.error(error)
+                    
+                }
+            }else{
             setResultSearch([])
-            setIsShowResultBox(false)
+           setIsShowResultBox(false)
+            }
         }
-     
+        searcher()
     },[inputValue])
     console.log(inputValue);
     console.log(resultSearch);
@@ -50,9 +51,9 @@ export default function ContainerResultSearch() {
 	<div  className={`${isShowResultBox ? "block" : "hidden"} w-full h-max bg-sky-50 border border-indigo-200 shadow-lg`}>
 	<ul className='w-full  h-max max-h-72 overflow-y-auto p-1 text-sm md:text-base xl:text-lg '>
 	{ resultSearch.map(item=>
-    <li className='w-full px-1 md:px-2  max-xs:h-7 h-10 md:h-12 max-xs:pt-1 pt-[10px]   hover:bg-blue-200 hover:opacity-100 overflow-hidden  cursor-pointer opacity-70 truncate'>
+    <li key={item._id} className='w-full px-1 md:px-2  max-xs:h-7 h-10 md:h-12 max-xs:pt-1 pt-[10px]   hover:bg-blue-200 hover:opacity-100 overflow-hidden  cursor-pointer opacity-70 truncate'>
 	<svg className='w-4 h-4 xl:w-5 xl:h-5 mr-1  inline-block' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"></path></svg>
-	{item.name}</li>
+	{item.brand_name}</li>
     ) }
 	</ul>
 

9bd51c8d81f992d79369feb6f3936bd950d09ab7
mo3ito <mostafa.tar90@gmail.com>
2023-12-26 23:10:16 +0330

    repair socket

--- components/rollReports/statisticsDisplay.tsx
+++ components/rollReports/statisticsDisplay.tsx
@@ -6,47 +6,57 @@ export default function StatisticsDisplay() {
 
   const [socketEyeRoll, setSocketEyeRoll] = useState<Socket | null>(null);
   const [seenUserEyeRollPage, setSeenUserEyeRollPage] = useState<number>(0);
-  useEffect(() => {
-    const newSocket = io("http://localhost:5002");
-    setSocketEyeRoll(newSocket);
-    return () => {
-      newSocket.disconnect();
-    };
-  }, []);
+  // useEffect(() => {
+  //   const newSocket = io("http://localhost:5002");
+  //   setSocketEyeRoll(newSocket);
+  //   return () => {
+  //     newSocket.disconnect();
+  //   };
+  // }, []);
 
-  useEffect(() => {
-    if (socketEyeRoll) {
+  // useEffect(() => {
+  //   if (socketEyeRoll) {
 
-      socketEyeRoll.on("updateSeenUser", (updatedSeenUser) => {
-        setSeenUserEyeRollPage(updatedSeenUser);
-      });
-    }
-  }, [socketEyeRoll]);
+  //     socketEyeRoll.on("updateSeenUser", (updatedSeenUser) => {
+  //       setSeenUserEyeRollPage(updatedSeenUser);
+  //     });
+  //   }
+  // }, [socketEyeRoll]);
 
-  console.log("seenUserEyeRollPage:", seenUserEyeRollPage);
+  // console.log("seenUserEyeRollPage:", seenUserEyeRollPage);
 
-  const [socketOnlineMenu , setSocketOnlineMenu]=useState<Socket|null>(null)
-  const [seenUserOnlineMenuPage, setSeenUserOnlineMenuPage] = useState<number>(0);
+  // useEffect(() => {
+  //   if (socketEyeRoll) {
+  //     socketEyeRoll.on("updateSeenUser", (updatedSeenUser) => {
+  //       setSeenUserEyeRollPage(updatedSeenUser);
+  //     });
+  //   } else {
+  //     console.error("Socket connection not established.");
+  //   }
+  // }, [socketEyeRoll]);
 
-  useEffect(()=>{
-    const onlineMenuSocket = io("http://localhost:5001")
-    setSocketOnlineMenu(onlineMenuSocket)
+  // const [socketOnlineMenu , setSocketOnlineMenu]=useState<Socket|null>(null)
+  // const [seenUserOnlineMenuPage, setSeenUserOnlineMenuPage] = useState<number>(0);
 
-    return ()=>{
-      socketOnlineMenu?.disconnect()
-    }
+  // useEffect(()=>{
+  //   const onlineMenuSocket = io("http://localhost:5001")
+  //   setSocketOnlineMenu(onlineMenuSocket)
 
-  },[])
+  //   return ()=>{
+  //     socketOnlineMenu?.disconnect()
+  //   }
 
-  useEffect(()=>{
+  // },[])
 
-    if(socketOnlineMenu){
-      socketOnlineMenu.on("updateSeenUser",(updateSeenUser)=>{
-        setSeenUserOnlineMenuPage(updateSeenUser)
-      })
-    }
+  // useEffect(()=>{
 
-  },[socketOnlineMenu])
+  //   if(socketOnlineMenu){
+  //     socketOnlineMenu.on("updateSeenUser",(updateSeenUser)=>{
+  //       setSeenUserOnlineMenuPage(updateSeenUser)
+  //     })
+  //   }
+
+  // },[socketOnlineMenu])
 
 
 
@@ -65,7 +75,7 @@ export default function StatisticsDisplay() {
     </div>
 
     <div className=' max-xs:w-full w-44   lg:w-56 xl:w-72 xl:h-[330px]  bg-indigo-100 cursor-pointer rounded-2xl hover:scale-105 duration-200 flex items-center justify-around  flex-col py-10 px-4'>
-    <div className='text-4xl font-semibold'>{seenUserOnlineMenuPage}</div>
+    <div className='text-4xl font-semibold'>677</div>
     <svg className='w-20 h-20 fill-red-500' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12.001 4.52853C14.35 2.42 17.98 2.49 20.2426 4.75736C22.5053 7.02472 22.583 10.637 20.4786 12.993L11.9999 21.485L3.52138 12.993C1.41705 10.637 1.49571 7.01901 3.75736 4.75736C6.02157 2.49315 9.64519 2.41687 12.001 4.52853ZM18.827 6.1701C17.3279 4.66794 14.9076 4.60701 13.337 6.01687L12.0019 7.21524L10.6661 6.01781C9.09098 4.60597 6.67506 4.66808 5.17157 6.17157C3.68183 7.66131 3.60704 10.0473 4.97993 11.6232L11.9999 18.6543L19.0201 11.6232C20.3935 10.0467 20.319 7.66525 18.827 6.1701Z"></path></svg>
     <p className='w-full h-max text-center font-semibold'>view my online menu page</p>
     </div>

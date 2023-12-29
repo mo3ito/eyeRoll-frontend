a5a481d53c87906044df03074306e48b3b19fa77
mo3ito <mostafa.tar90@gmail.com>
2023-12-29 09:40:52 +0330

    change rollReports to eyeRoll Reports

--- routes/dashboardRoutes/dashboardItem.ts
+++ routes/dashboardRoutes/dashboardItem.ts
@@ -17,18 +17,16 @@ const accordionItems = [
         options: [
       // { id: uuidv4(), name: "roll setting" , childOption:[{"id":uuidv4() , childName:"discount range" , href: "/business-owner-dashboard/discount-setting"},] },
       { id: uuidv4(), href: "/business-owner-dashboard/discount-setting", name: "set roll" },
-      { id: uuidv4(), href: "/business-owner-dashboard/roll-reports", name: "roll reports" },
       { id: uuidv4(), href: "/business-owner-dashboard/qr-code-link-roll", name: "roll QR Code link" , },
     ],
   },
-  // {
-  //   id: uuidv4(),
-  //   title: "setting",
-  //   options: [
-  //     { id: uuidv4(), name: "roll setting" , childOption:[{"id":uuidv4() , childName:"discount range" , href: "/business-owner-dashboard/discount-setting"},] },
-  //     { id: uuidv4(), href: "#", name: "review setting" },
-  //   ],
-  // },
+  {
+    id: uuidv4(),
+    title:"Reports",
+        options: [
+      { id: uuidv4(), href: "/business-owner-dashboard/eyeRoll-reports", name: "all reports" },
+    ],
+  },
   {
     id: uuidv4(),
     title: "online menu",

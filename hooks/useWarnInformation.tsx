bb23002e18fa9fee6cb70538507fe0c4071ac470
mo3ito <mostafa.tar90@gmail.com>
2024-01-19 12:55:23 +0330

    update useWarnInformation

--- hooks/useWarnInformation.tsx
+++ hooks/useWarnInformation.tsx
@@ -1,27 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { toast } from "react-toastify";
 
 interface InfosType {
-  is_complete_information: boolean; 
-  }
-  
-
- const useWarnInformation = (infos : InfosType )=>{
-    useEffect(() => {
-        let interValTime: NodeJS.Timeout;
-        if (infos && !infos?.is_complete_information) {
-          interValTime = setInterval(() => {
-            toast.warn(
-              "please fill you information from menu -> information -> editting information"
-            );
-          }, 1200000);
-        }
-
-      
-    
-        return () => clearInterval(interValTime);
-      }, [infos]);
-
+  is_complete_information: boolean;
 }
+
+const useWarnInformation = (infos: InfosType) => {
+  
+  const hasWarnedIsCompleteInformationRef = useRef<boolean>(false);
+
+  useEffect(() => {
+    const warn = () => {
+      if (!hasWarnedIsCompleteInformationRef.current && infos && !infos?.is_complete_information) {
+        toast.warn(
+          "please fill your information from menu -> information -> editing information"
+        );
+        hasWarnedIsCompleteInformationRef.current = true;
+      }
+    };
+
+    let intervalTime: NodeJS.Timeout;
+    intervalTime = setInterval(() => {
+      warn();
+      hasWarnedIsCompleteInformationRef.current = false; 
+    }, 30000);
+
+    warn();
+
+    return () => clearInterval(intervalTime);
+  }, [infos]);
+};
 
 export default useWarnInformation;

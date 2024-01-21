8b5c70fb3be1d165e6c57e499c1233ed87528c1d
mo3ito <mostafa.tar90@gmail.com>
2024-01-21 11:13:13 +0330

    add h-max on registeration

--- app/(registeration)/layout.tsx
+++ app/(registeration)/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="overflow-x-hidden inset-0 w-screen bg-sky-100">
+      <body className="overflow-x-hidden min-h-screen h-max inset-0 w-screen bg-sky-100">
         <AuthContextProvider>
           <ReactQueryProvider>
             <HomePageHeader />

ade2991e65b27bf2a1ff872c9cf715df78e10bc6
mo3ito <mostafa.tar90@gmail.com>
2024-01-26 15:48:40 +0330

    add condition for admin-eyeRoll path on middleware

--- middleware.ts
+++ middleware.ts
@@ -6,15 +6,24 @@ import jwt_decode from "jwt-decode";
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const token = await request.cookies.get(EYEROLL_TOKEN)?.value;
+  
   if (!token && pathname.startsWith("/business-owner-dashboard")) {
     return NextResponse.redirect(
       new URL("/register-business-owner/login", request.url)
     );
   }
+
+  if(!token && pathname.startsWith("/admin-eyeRoll")){
+    return NextResponse.redirect(
+      new URL("/register-admin/login",request.url)
+    )
+  }
+
   if (token) {
     try {
       const informationToken = (await jwt_decode(token)) as {
         is_businessOwner: boolean;
+        is_admin:boolean
       };
       if (
         pathname.startsWith("/business-owner-dashboard") &&
@@ -23,6 +32,11 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(
           new URL("/register-business-owner/login", request.url)
         );
+      } else if(pathname.startsWith("/admin-eyeRoll") && !informationToken.is_admin ){
+        return NextResponse.redirect(
+          new URL("/register-admin/login",request.url)
+        )
+       
       }
     } catch (error) {
       console.error("error:", error);

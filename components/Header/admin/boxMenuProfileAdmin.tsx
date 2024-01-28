936e0fd0d3abb79397ad4a7b6221af8e27d0e45d
mo3ito <mostafa.tar90@gmail.com>
2024-01-28 09:25:26 +0330

    requests option on boxMenuProfileAdmin

--- components/Header/admin/boxMenuProfileAdmin.tsx
+++ components/Header/admin/boxMenuProfileAdmin.tsx
@@ -39,6 +39,7 @@ export default function BoxMenuProfileAdmin({setShowAside , setShowBox  , showBo
           </div>
           <div className={` ${showBox ? 'absolute' : 'hidden'}  bg-blue-100 max-xs:w-52 w-60  h-max top-12 sm:top-14  border rounded-lg border-purple-400`}>
           <ul className="w-full h-max  max-xs:text-xs text-sm sm:text-base p-1 shadow-md">
+          <li onClick={()=>logoutHandler(router , setInfos)} className=' cursor-pointer px-2 py-2 hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white '>requests</li>
           <li onClick={()=>logoutHandler(router , setInfos)} className=' cursor-pointer px-2 py-2 hover:bg-pink-300 rounded-lg text-fuchsia-700 hover:font-semibold hover:text-white '>log out</li>
           </ul>
           </div>
